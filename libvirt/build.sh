8fcdbbbde49bfd1bf515618cea1ca2186457ea2e
Your Name <you@example.com>
2025-04-06 20:31:24 +0100

    correctd docker build for 4

--- libvirt/build.sh
+++ libvirt/build.sh
@@ -5,11 +5,13 @@ SCRIPT_DIR=`dirname ${SCRIPT}`
 cd ${SCRIPT_DIR}/..
 
 IMAGE="libvirt"
-FROM=ubuntu:jammy
-RELEASE=dalmatian
 VERSION=${VERSION:-latest}
 DISTRO=${DISTRO:-ubuntu}
-REGISTRY_URI=${REGISTRY_URI:-"sainusahib"}
+DISTRO_VERSION=${DISTRO_VERSION:-jammy}
+
+REGISTRY_URI=${REGISTRY_URI:-"sainusahib/"}
+EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
+
 
 # Check and setup builder
 TAG_INFO=${1:-"latest"}
@@ -26,18 +28,21 @@ fi
 docker login docker.io -u $DOCKER_USER -p $DOCKER_TOKEN
 docker login ghcr.io -u $GHCR_USER -p $GHCR_TOKEN
 
-# Build for multiple architectures
 docker buildx build \
-    --platform linux/amd64,linux/arm64 \
-    -f ${IMAGE}/Dockerfile.${DISTRO} \
-    --network=host \
-    --push \
+            --file=${IMAGE}/Dockerfile.${DISTRO} \
+            --platform linux/amd64,linux/arm64 \
+             --network=host \
+             --push \
+             --build-arg="FROM=${DISTRO}:${DISTRO_VERSION}" \
+             --build-arg="CEPH_RELEASE=${CEPH_RELEASE}" \
+             --build-arg="CEPH_RELEASE_TAG=${CEPH_RELEASE_TAG}" \
+             --build-arg="CEPH_REPO=${CEPH_REPO}" \
+             --build-arg="CEPH_KEY=${CEPH_KEY}" \
             --cache-from type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache \
             --cache-to type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache,mode=max \
              --tag=${REGISTRY_URI}/${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
              --tag=docker.io/${REGISTRY_URI}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
              --tag=ghcr.io/${GHCR_USER}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
-    ${extra_build_args} \
-    ${IMAGE}
+             ${extra_build_args} ${IMAGE}
 
 cd -
--- mariadb/build.sh
+++ mariadb/build.sh
@@ -11,19 +11,38 @@ DISTRO_VERSION=${DISTRO_VERSION:-jammy}
 
 REGISTRY_URI=${REGISTRY_URI:-"sainusahib/"}
 EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
-# Remove any existing builder
-docker buildx rm multi-arch-builder || true
 
-# Create and use new builder
-docker buildx create --driver docker-container --use
+# Check and setup builder
+TAG_INFO=${1:-"latest"}
+DOCKER_USER=${2:-""}
+DOCKER_TOKEN=${3:-""}
+GHCR_USER=${4:-""}
+GHCR_TOKEN=${5:-""}
 
-# Build for multiple architectures
+if ! docker buildx ls | grep -q multi-arch-builder; then
+    docker buildx create --driver docker-container --name multi-arch-builder --use
+else
+    docker buildx use multi-arch-builder
+fi
+docker login docker.io -u $DOCKER_USER -p $DOCKER_TOKEN
+docker login ghcr.io -u $GHCR_USER -p $GHCR_TOKEN
+
 docker buildx build \
-    --platform linux/amd64,linux/arm64 \
-    -f ${IMAGE}/Dockerfile.${DISTRO} \
-    --network=host \
-    -t ${REGISTRY_URI}${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}\
-    ${extra_build_args} \
-    ${IMAGE}
+            --file=${IMAGE}/Dockerfile.${DISTRO} \
+            --platform linux/amd64,linux/arm64 \
+             --network=host \
+             --push \
+             --build-arg="FROM=${DISTRO}:${DISTRO_VERSION}" \
+             --build-arg="CEPH_RELEASE=${CEPH_RELEASE}" \
+             --build-arg="CEPH_RELEASE_TAG=${CEPH_RELEASE_TAG}" \
+             --build-arg="CEPH_REPO=${CEPH_REPO}" \
+             --build-arg="CEPH_KEY=${CEPH_KEY}" \
+            --cache-from type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache \
+            --cache-to type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache,mode=max \
+             --tag=${REGISTRY_URI}/${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+             --tag=docker.io/${REGISTRY_URI}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+             --tag=ghcr.io/${GHCR_USER}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+             ${extra_build_args} ${IMAGE}
 
 cd -
+
