9013347eb134fd0f614b2aa20cdb3658c06c33fc
Your Name <you@example.com>
2025-04-08 22:32:10 +0100

    kubernetes-entrypoint multicarch

--- kubernetes-entrypoint/build.sh
+++ kubernetes-entrypoint/build.sh
@@ -1,48 +1,43 @@
 #!/bin/bash
-SCRIPT=`realpath $0`
-SCRIPT_DIR=`dirname ${SCRIPT}`
+# SCRIPT=`realpath $0`
+# SCRIPT_DIR=`dirname ${SCRIPT}`
 ## Only build from main folder
-cd ${SCRIPT_DIR}/..
+# cd ${SCRIPT_DIR}/..
 
-IMAGE="kubernetes-entrypoint"
-VERSION=${VERSION:-latest}
-DISTRO=${DISTRO:-ubuntu}
-DISTRO_VERSION=${DISTRO_VERSION:-jammy}
+# IMAGE="kubernetes-entrypoint"
+# VERSION=${VERSION:-latest}
+# DISTRO=${DISTRO:-ubuntu}
+# DISTRO_VERSION=${DISTRO_VERSION:-jammy}
 
-REGISTRY_URI=${REGISTRY_URI:-"sainusahib"}
-EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
+# REGISTRY_URI=${REGISTRY_URI:-"sainusahib"}
+# EXTRA_TAG_INFO=${EXTRA_TAG_INFO:-""}
 
-# Check and setup builder
+# # Check and setup builder
 TAG_INFO=${1:-"latest"}
 DOCKER_USER=${2:-""}
 DOCKER_TOKEN=${3:-""}
 GHCR_USER=${4:-""}
 GHCR_TOKEN=${5:-""}
 
-if ! docker buildx ls | grep -q multi-arch-builder; then
-    docker buildx create --driver docker-container --name multi-arch-builder --use
-else
-    docker buildx use multi-arch-builder
-fi
-docker login docker.io -u $DOCKER_USER -p $DOCKER_TOKEN
-docker login ghcr.io -u $GHCR_USER -p $GHCR_TOKEN
+# docker login docker.io -u $DOCKER_USER -p $DOCKER_TOKEN
+# docker login ghcr.io -u $GHCR_USER -p $GHCR_TOKEN
 
-docker buildx build \
-            --file=${IMAGE}/Dockerfile.${DISTRO} \
-            --platform linux/amd64,linux/arm64 \
-             --network=host \
-             --push \
-             --build-arg="FROM=${DISTRO}:${DISTRO_VERSION}" \
-             --build-arg="CEPH_RELEASE=${CEPH_RELEASE}" \
-             --build-arg="CEPH_RELEASE_TAG=${CEPH_RELEASE_TAG}" \
-             --build-arg="CEPH_REPO=${CEPH_REPO}" \
-             --build-arg="CEPH_KEY=${CEPH_KEY}" \
-            --cache-from type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache \
-            --cache-to type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache,mode=max \
-             --tag=${REGISTRY_URI}/${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
-             --tag=docker.io/${REGISTRY_URI}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
-             --tag=ghcr.io/${GHCR_USER}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
-             ${extra_build_args} ${IMAGE}
+# docker buildx build \
+#             --file=${IMAGE}/Dockerfile.${DISTRO} \
+#             --platform linux/amd64,linux/arm64 \
+#              --push \
+#             --cache-from type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache \
+#             --cache-to type=registry,ref=${REGISTRY_URI}/${IMAGE}:cache,mode=max \
+#              --tag=${REGISTRY_URI}/${IMAGE}:${VERSION}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+#              --tag=docker.io/${REGISTRY_URI}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+#              --tag=ghcr.io/${GHCR_USER}/${IMAGE}:${TAG_INFO}-${DISTRO}_${DISTRO_VERSION}${EXTRA_TAG_INFO} \
+#              ${extra_build_args} ${IMAGE}
 
-cd -
+# cd -
 
+git clone --depth=1 https://github.com/airshipit/kubernetes-entrypoint.git src
+cd src
+docker buildx build --platform linux/arm64,linux/amd64 \
+    -f images/Dockerfile.ubuntu_jammy \
+    -t ghcr.io/${GHCR_USER}/kubernetes-entrypoint:latest-ubuntu_focal \
+    --push .
